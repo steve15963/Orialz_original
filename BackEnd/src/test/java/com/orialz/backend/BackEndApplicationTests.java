091cfc7c5d9f82e3a99eb77116314bb3d6272b57
송진현 <dkdkdkal123@naver.com>
2023-09-14 10:08:56 +0900

    [TEST] 테스트 코드 비 활성화

--- BackEnd/src/test/java/com/orialz/backend/BackEndApplicationTests.java
+++ BackEnd/src/test/java/com/orialz/backend/BackEndApplicationTests.java
@@ -1,13 +1,15 @@
 package com.orialz.backend;
 
+/*
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackEndApplicationTests {
+/class BackEndApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
 }
+*/
