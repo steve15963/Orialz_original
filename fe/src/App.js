4d95345c1225c4cf3bc49f71f39b9b67eaa92019
slemfkdlqm <slemfkdlqm@naver.com>
2023-09-20 13:23:58 +0900

    [Feat]: eslint 해결

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable array-callback-return */
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/Login";
