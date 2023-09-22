cd04dc81167550964cbb79172ffa9545cb89d02d
dldntjr0516@gmail.com <dldntjr0516@gmail.com>
2023-09-22 17:37:50 +0900

    [Feat] YOLO Train Test

---
+++ AI/yolov8-test/train_practice.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+from multiprocessing import freeze_support
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+def run():
+    freeze_support()
+    
+    # Load a model 
+    model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+    # Use the model
+    model.train(data="C:\SSAFY\project-special\YoLO-test\yolov8-test\Aquarium\data.yaml", epochs=3, batch=8)  # train the model
+    print(type(model.names), len(model.names))
+    print(model.names)
+    
+    # metrics = model.val()  # evaluate model performance on the validation set
+    results = model.predict(source="./Aquarium/test/images", save=True)  # predict on an image
+    # path = model.export(format="onnx")  # export the model to ONNX format
+    # print(results)
+
+if __name__ == '__main__':
+    run()
