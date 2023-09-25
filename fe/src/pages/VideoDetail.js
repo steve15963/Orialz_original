bd96464ef433327d2a3b047a96056a4945037cc3
slemfkdlqm <slemfkdlqm@naver.com>
2023-09-25 12:45:30 +0900

    에러테스트

--- fe/src/pages/VideoDetail.js
+++ fe/src/pages/VideoDetail.js
@@ -9,26 +9,19 @@ import axios from 'axios';
 const VideoDetail = () => {
 	const playerRef = React.useRef(null);
 	console.log(data3);
-	console.log(1);
 	async function getData() {
-		console.log(2);
 		try {
 			//응답 성공
-			console.log(3);
 			const response = await axios.get("https://test.orialz.com/hls/streaming/3/output.m3u8",{
 			});
-			console.log(4);
 			console.log(response);
 		} catch (error) {
-			console.log(5);
 			//응답 실패
 			console.error(error);
-			console.log(6);
 		}
 	}
-	console.log(7);
 	getData();
-	console.log(8);
+
     const videoJsOptions = {
 		autoplay: true,
 		controls: true,
@@ -49,20 +42,18 @@ const VideoDetail = () => {
     };
 	
 	const handlePlayerReady = (player) => {
-		console.log(9);
 		playerRef.current = player;
-		console.log(10);
+	
 		// You can handle player events here, for example:
 		player.on('waiting', () => {
 		  videojs.log('player is waiting');
 		});
-		console.log(11);
+	
 		player.on('dispose', () => {
 		  videojs.log('player will dispose');
 		});
-		console.log(12);
 	};
-	console.log(13);
+
 	
 	return (
 		<div className="video-detail-container">
