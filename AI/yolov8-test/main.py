402de8b070ebe26ee0afd0a8a6d5529e2f566061
dldntjr0516@gmail.com <dldntjr0516@gmail.com>
2023-09-26 15:58:58 +0900

    [Feat] 데이터 학습 준비 완료

--- AI/yolov8-test/main.py
+++ AI/yolov8-test/main.py
@@ -7,14 +7,15 @@ def run():
     freeze_support()
     
     # Load a model
-    model = YOLO("yolov8n.yaml")  # build a new model from scratch
-    model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+    # model = YOLO("yolov8n.yaml")  # build a new model from scratch
+    # model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("best.pt")
 
     # Use the model
-    model.train(data="coco128.yaml", epochs=3, batch=8)  # train the model
-    metrics = model.val()  # evaluate model performance on the validation set
-    results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
-    path = model.export(format="onnx")  # export the model to ONNX format
+    # model.train(data="coco128.yaml", epochs=3, batch=8)  # train the model
+    # metrics = model.val()  # evaluate model performance on the validation set
+    results = model("./images/fish002.jpg")  # predict on an image
+    # path = model.export(format="onnx")  # export the model to ONNX format
     print(results)
 
 if __name__ == '__main__':
