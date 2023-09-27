f6a37281d7fb04fa6cd3bd80f6962b41f5deff4e
cvivis <hsy0754@naver.com>
2023-09-27 09:19:45 +0900

    [Feat] 인공지능 서버에 요청 보내기

---
+++ Split/src/main/java/com/example/split/video/controller/dto/response/TestDto.java
@@ -0,0 +1,15 @@
+package com.example.split.video.controller.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class TestDto {
+    private String text;
+}
