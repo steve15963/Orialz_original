d5538c65bbf18ba2b02a76a0aba874abc8245ea6
slemfkdlqm <slemfkdlqm@naver.com>
2023-09-27 13:22:30 +0900

    [Feat] 검색 api 연동
    Former-commit-id: 14606edeba05a71249d8fdaead505ebcff5506f5

--- fe/src/pages/Main.js
+++ fe/src/pages/Main.js
@@ -1,28 +1,43 @@
-import { useEffect} from "react";
+import { useState, useEffect } from "react";
 import VideoContainer from "../components/videoContainer/VideoContainer";
 import "./Main.css";
 import Header from "../components/header/Header";
+import axios from "axios";
 
 export default function Main({videos}) {
+
+	const [searchedVideos, setSearchedVideos] = useState([]);
 
 	useEffect(() => {
 		const params = new URLSearchParams(window.location.search);
 		const token = params.get("token");
-
-		if (token) {
-		localStorage.setItem("access_token", token);
-		window.location.replace("/");
-		}
-
 		
-	}, []);
+		if (token) {
+			localStorage.setItem("access_token", token);
+			window.location.replace("/");
+		}
+		
+		setSearchedVideos(videos)
+		
+	}, [videos]);
 
-	
+
+	async function searchVideos(keyword){
+		try {
+			//응답 성공
+			const response = await axios.get(`https://test.orialz.com/api/video/search?keyword=${keyword}`, {});
+			console.log(response);
+			setSearchedVideos(response.data);
+		} catch (error) {
+			//응답 실패
+			console.error(error);
+		}
+	}
 
 	return (
 		<div className="main">
-			<Header></Header>
-			<VideoContainer videos={videos}></VideoContainer>
+			<Header searchVideos={searchVideos}></Header>
+			<VideoContainer videos={searchedVideos}></VideoContainer>
 		</div>
 	);
 }
