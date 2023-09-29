00861b94abf71dd74f43ac4b6a9dc469e16018ad
송진현 <dkdkdkal123@naver.com>
2023-09-29 22:28:10 +0900

    [feat] 기본 Repository 완성
    Former-commit-id: 3ca87429cc3417cd5003964c9666fb0231bb7f21

---
+++ BackEnd/src/main/java/com/orialz/backend/blur/domain/repository/SettingKeywordRepository.java
@@ -0,0 +1,8 @@
+package com.orialz.backend.blur.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.orialz.backend.blur.domain.entity.SettingKeyword;
+
+public interface SettingKeywordRepository extends JpaRepository<SettingKeyword,Long> {
+}
