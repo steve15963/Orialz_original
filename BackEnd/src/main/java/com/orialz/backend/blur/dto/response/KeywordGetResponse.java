eb20b47010666d7c678a806215de5efe6e16906f
송진현 <dkdkdkal123@naver.com>
2023-10-01 12:14:36 +0900

    [feat] 추가 Vo 작성.
    Former-commit-id: e708b1a356f485d431c38e8bcfde0efa96ff078f

---
+++ BackEnd/src/main/java/com/orialz/backend/blur/dto/response/KeywordGetResponse.java
@@ -0,0 +1,17 @@
+package com.orialz.backend.blur.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class KeywordGetResponse {
+	long id;
+	String keyword;
+}
