47ebea6d719a22ff8a8b36c5712eacf0112482ab
송진현 <dkdkdkal123@naver.com>
2023-10-01 12:15:30 +0900

    [feat] OnKeyword, FindKeyword Rest API작성을 위하여 Repository작성.
    Former-commit-id: 9456c395cfe9da793287101e32906b3a190cea6e

--- BackEnd/src/main/java/com/orialz/backend/blur/domain/repository/FindKeywordRepository.java
+++ BackEnd/src/main/java/com/orialz/backend/blur/domain/repository/FindKeywordRepository.java
@@ -1,10 +1,23 @@
 package com.orialz.backend.blur.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.orialz.backend.blur.domain.entity.FindKeyword;
 
 public interface FindKeywordRepository extends JpaRepository<FindKeyword,Long> {
-
-
+	@Query("SELECT f "
+		+ "FROM FindKeyword as f "
+		+ "WHERE f.video.videoId = :vId "
+		+ "AND f.keyword IN ("
+		+ "SELECT o.keyword "
+		+ "FROM OnKeyword as o "
+		+ "WHERE o.member.id = :mId "
+		+ "ORDER BY f.time asc "
+		+ ")"
+	)
+	List<FindKeyword> findAllBymIdAndvId(@Param("mId") long mId,   @Param("vId") long vId);
 }
