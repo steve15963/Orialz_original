7e8788f032153e10447407783bcd1d5d37b2ee87
송진현 <dkdkdkal123@naver.com>
2023-10-01 12:10:56 +0900

    [Refact] Update Vo
    Former-commit-id: 6fab854b874d3d625f2d69634477cae16abe3927

--- BackEnd/src/main/java/com/orialz/backend/blur/domain/repository/SettingKeywordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.orialz.backend.blur.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.orialz.backend.blur.domain.entity.SettingKeyword;
-
-public interface SettingKeywordRepository extends JpaRepository<SettingKeyword,Long> {
-}
