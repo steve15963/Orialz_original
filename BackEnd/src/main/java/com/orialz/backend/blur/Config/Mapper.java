52b6f650bee644ce0b1e6092bf076ef6af02efec
송진현 <dkdkdkal123@naver.com>
2023-10-01 12:11:38 +0900

    [Feat] Mapper 추가.
    Former-commit-id: ee4bd28886c229428c5ed4a40fdf4e60edc1870d

---
+++ BackEnd/src/main/java/com/orialz/backend/blur/Config/Mapper.java
@@ -0,0 +1,49 @@
+package com.orialz.backend.blur.Config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.orialz.backend.blur.domain.entity.FindKeyword;
+import com.orialz.backend.blur.domain.entity.Keyword;
+import com.orialz.backend.blur.domain.entity.OnKeyword;
+import com.orialz.backend.blur.dto.response.FindKeywordGetResponse;
+import com.orialz.backend.blur.dto.response.KeywordGetResponse;
+import com.orialz.backend.blur.dto.response.OnKeywordGetResponse;
+
+@Configuration
+public class Mapper {
+	@Bean
+	public ModelMapper OnKeywordToOnKeywordReponseMapper(){
+		ModelMapper mp = new ModelMapper();
+		mp.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		mp.createTypeMap(OnKeyword.class, OnKeywordGetResponse.class)
+			.addMapping(OnKeyword::getId,OnKeywordGetResponse::setId)
+			.addMapping(data -> data.getKeyword().getKeyword(),OnKeywordGetResponse::setKeyword);
+		return mp;
+	}
+
+	@Bean
+	public ModelMapper KeywordToKeywordReponseMapper(){
+		ModelMapper mp = new ModelMapper();
+		mp.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		mp.createTypeMap(Keyword.class, KeywordGetResponse.class)
+			.addMapping(Keyword::getId,KeywordGetResponse::setId)
+			.addMapping(Keyword::getKeyword,KeywordGetResponse::setKeyword);
+		return mp;
+	}
+	@Bean
+	public ModelMapper FindKeywordToFindKeywordReponseMapper(){
+		ModelMapper mp = new ModelMapper();
+		mp.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		mp.createTypeMap(FindKeyword.class, FindKeywordGetResponse.class)
+			.addMapping(FindKeyword::getLabel, FindKeywordGetResponse::setLabel)
+			.addMapping(FindKeyword::getMinX,FindKeywordGetResponse::setX)
+			.addMapping(FindKeyword::getMinY,FindKeywordGetResponse::setY)
+			.addMapping(FindKeyword::getW,FindKeywordGetResponse::setW)
+			.addMapping(FindKeyword::getH,FindKeywordGetResponse::setH);
+		return mp;
+	}
+}
+
