2df5a88da663bf519b64281756f07d296bafb9b9
송진현 <dkdkdkal123@naver.com>
2023-10-03 16:38:45 +0900

    [test] 어노테이션 추가.
    Former-commit-id: 6444385c473f1a746154a90d0fb4f6b034692c72

--- FFMPEGServer/src/main/java/com/orialz/ffmpegserver/video/Vo/Entity/FindKeyword.java
+++ FFMPEGServer/src/main/java/com/orialz/ffmpegserver/video/Vo/Entity/FindKeyword.java
@@ -9,8 +9,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -18,6 +20,8 @@ import lombok.ToString;
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FindKeyword {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
