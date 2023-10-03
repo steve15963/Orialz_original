fd1ada1f3bf727f9c6093661e4ef6dca64f89f03
송진현 <dkdkdkal123@naver.com>
2023-10-03 15:41:19 +0900

    [feat] AI FFMPEG SpringServer 생성.
    Former-commit-id: 69533a7eb2c9beebaeabdb2c9ce80badd0fcc977

---
+++ FFMPEGServer/src/main/java/com/orialz/ffmpegserver/Common/Entity/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.orialz.ffmpegserver.Common.Entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+}
---
+++ FFMPEGServer/src/main/java/com/orialz/ffmpegserver/video/hadoop/HadoopControl.java
@@ -0,0 +1,66 @@
+package com.orialz.ffmpegserver.video.hadoop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.orialz.ffmpegserver.video.Config.HadoopConfiguration;
+import com.orialz.ffmpegserver.video.Vo.Entity.Job;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class HadoopControl {
+
+	HadoopConfiguration hadoopConfiguration;
+
+	@Value("{hadoop.root}")
+	static String root;
+
+
+	public String HDFSUploadInputTxt(Job job) throws IOException {
+		FileSystem fileSystem = hadoopConfiguration.getFileSystem();
+		String local = job.getLocalInputPath();
+		String remote = job.getRemoteInputPath();
+		fileSystem.copyFromLocalFile(new Path(local),new Path(root+remote));
+		return root+remote;
+	}
+
+	public boolean HDFSDownloadOutputTxt(Job job) throws IOException {
+		FileSystem fileSystem = hadoopConfiguration.getFileSystem();
+		String local = job.getRemoteOutputPath();
+		String remote = job.getLocalOutputPath();
+		fileSystem.copyToLocalFile(new Path(root+remote),new Path(local));
+		return true;
+	}
+
+	public void MapreduceRunJob(Job nowJob) throws IOException, InterruptedException {
+		ProcessBuilder processBuilder = new ProcessBuilder(
+			"hadoop",
+			"jar",
+			"/home/hadoop/jenkins/workspace/hadoop/build/libs/Hadoop-Gradle-1.0-SNAPSHOT.jar",
+			nowJob.getRemoteInputPath(),
+			nowJob.getRemoteOutputPath()
+		);
+		Process process = processBuilder.start();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		String line;
+		while ((line = reader.readLine()) != null) {
+			log.info("data : {}",line);
+		}
+		int exitCode = process.waitFor();
+	}
+
+	public void RemoveLocalInput(){
+	}
+
+	public void RemoveLocalOutput(){
+
+	}
+}
