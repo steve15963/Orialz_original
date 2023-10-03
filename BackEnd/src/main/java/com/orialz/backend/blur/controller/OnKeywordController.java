9cafeebb88593989cbbfd22f4e178ab47e6fd567
송진현 <dkdkdkal123@naver.com>
2023-10-03 21:14:37 +0900

    [Feat] 컨트롤러 세분화
    Former-commit-id: 8ffffc4f946b1bc4f467569392b3dd2fe8c8fd0b

--- BackEnd/src/main/java/com/orialz/backend/blur/controller/OnKeywordController.java
+++ BackEnd/src/main/java/com/orialz/backend/blur/controller/OnKeywordController.java
@@ -1,6 +1,7 @@
 package com.orialz.backend.blur.controller;
 
 import java.net.http.HttpResponse;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.orialz.backend.blur.dto.response.KeywordGetResponse;
+import com.orialz.backend.blur.dto.response.OnKeywordGetResponse;
 import com.orialz.backend.blur.service.OnKeywordService;
 
 import lombok.AllArgsConstructor;
@@ -18,6 +21,12 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/api/onkeyword")
 public class OnKeywordController {
 	OnKeywordService onKeywordService;
+
+	@GetMapping("/list/{id}")
+	public ResponseEntity<List<OnKeywordGetResponse>> getSettingKeyword(@PathVariable long id){
+		return new ResponseEntity<>(onKeywordService.SettingKeywordList(id),HttpStatus.OK);
+	}
+
 	@GetMapping("/change/{mId}/{kId}")
 	public ResponseEntity<HttpResponse> ChangeOnKeywordController(@PathVariable long mId, @PathVariable Long kId){
 		if(onKeywordService.ChangeOnKeyword(mId,kId)){
