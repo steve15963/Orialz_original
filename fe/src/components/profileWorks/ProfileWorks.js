04fc8dd0cbe4aa15482b14ab5365e67a457a66fd
이지원 <dlwldnjs427@naver.com>
2023-10-04 10:42:59 +0900

    [Chore] 파일 들여쓰기 수정
    Former-commit-id: c6802d1f099e752717a124c2efdc8d50774d2cb0

--- fe/src/components/profileWorks/ProfileWorks.js
+++ fe/src/components/profileWorks/ProfileWorks.js
@@ -4,34 +4,42 @@ import ProfileVideoContainer from "../profileVideoContainer/ProfileVideoContaine
 import ProfileCommentsContainer from "../profileCommentsContainer/ProfileCommentsContainer";
 
 export default function ProfileWorks() {
-  const [mode, setMode] = useState(0);
+    const [mode, setMode] = useState(0);
 
-  function setModeOne() {
-    setMode(1);
-  }
-  function setModeZero() {
-    setMode(0);
-  }
+    function setModeOne() {
+        setMode(1);
+    }
+    function setModeZero() {
+        setMode(0);
+    }
 
-  return (
-    <div className="profile-works">
-      <div className="profile-works-btns">
-        <div
-          className={`profile-video-btn ${mode === 0 ? "active" : ""}`}
-          onClick={setModeZero}
-        >
-          영상
+    return (
+        <div className="profile-works">
+            <div className="profile-works-btns">
+                <div
+                    className={`profile-video-btn ${
+                        mode === 0 ? "active" : ""
+                    }`}
+                    onClick={setModeZero}
+                >
+                    영상
+                </div>
+                <div
+                    className={`profile-comments-btn ${
+                        mode === 1 ? "active" : ""
+                    }`}
+                    onClick={setModeOne}
+                >
+                    댓글
+                </div>
+            </div>
+            <div className="profile-works-containers">
+                {mode === 0 ? (
+                    <ProfileVideoContainer />
+                ) : (
+                    <ProfileCommentsContainer />
+                )}
+            </div>
         </div>
-        <div
-          className={`profile-comments-btn ${mode === 1 ? "active" : ""}`}
-          onClick={setModeOne}
-        >
-          댓글
-        </div>
-      </div>
-      <div className="profile-works-containers">
-        {mode === 0 ? <ProfileVideoContainer /> : <ProfileCommentsContainer />}
-      </div>
-    </div>
-  );
+    );
 }
