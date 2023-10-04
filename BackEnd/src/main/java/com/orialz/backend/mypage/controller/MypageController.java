6faaa1dbf03f0ae9d5035f2deb959592a496b6bd
이지원 <dlwldnjs427@naver.com>
2023-10-05 08:48:22 +0900

    [Fix] 댓글, 마이페이지 accessToken 검증 추가
    Former-commit-id: 6b58e96765ed2626828c2877c97ac083e9a47346

--- BackEnd/src/main/java/com/orialz/backend/mypage/controller/MypageController.java
+++ BackEnd/src/main/java/com/orialz/backend/mypage/controller/MypageController.java
@@ -1,15 +1,14 @@
 package com.orialz.backend.mypage.controller;
 
+import com.orialz.backend.Member.config.jwt.TokenProvider;
 import com.orialz.backend.mypage.dto.response.MypageCommentListResponseDto;
 import com.orialz.backend.mypage.dto.response.MypageListResponseDto;
 import com.orialz.backend.mypage.dto.response.MypageVideoListResponseDto;
 import com.orialz.backend.mypage.service.MypageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,11 +17,19 @@ import java.util.List;
 @RequestMapping("/api/mypage")
 public class MypageController {
     private final MypageService mypageService;
+    private final TokenProvider tokenProvider;
 
-    @GetMapping("/{memberId}") // 나중에 pathVariable 제거
-    public ResponseEntity<MypageListResponseDto> getMyVideo(@PathVariable Long memberId){
-        MypageListResponseDto response = mypageService.getMypage(memberId);
-        return ResponseEntity.ok(response);
+    @GetMapping("/mydata")
+    public ResponseEntity<MypageListResponseDto> getMyVideo(@RequestHeader(value = "Authorization")String token){
+        try {
+            String accessToken = token.split("Bearer ")[1];
+            Long memberId = tokenProvider.getMemberId(accessToken);
+            MypageListResponseDto response = mypageService.getMypage(memberId);
+            return ResponseEntity.ok(response);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
     }
 
 
