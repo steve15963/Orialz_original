cb599a6a50fa49a0d0548ffbe81fe07998c8dcb1
이지원 <dlwldnjs427@naver.com>
2023-10-04 10:52:24 +0900

    [Fix] 로그인 시 헤더 디자인 수정
    Former-commit-id: b139500617187243ecee2a2e4e502d86ddab871f

--- fe/src/components/profileBox/ProfileBox.js
+++ fe/src/components/profileBox/ProfileBox.js
@@ -1,28 +1,66 @@
-
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import NoticeContainer from "../noticeContainer/NoticeContainer";
-import "./ProfileBox.css"
+import ProfileContainer from "../profileContainer/ProfileContainer";
+import "./ProfileBox.css";
 
-export default function ProfileBox(){
-
+export default function ProfileBox() {
     const [noticeOn, setNoticeOn] = useState(false);
+    const [profileOn, setProfileOn] = useState(false);
 
-    function falsifyNoticeOn(){
+    const profileRef = useRef();
+
+    function falsifyNoticeOn() {
         setNoticeOn(false);
     }
 
-    return(
-        <div className='bell-profile-box'>
-            {noticeOn ? <NoticeContainer falsifyNoticeOn={falsifyNoticeOn}/> : null}
-            <img src="/bell.svg" alt="bell" className='bell-img icon-wiggle' onClick={()=>{
-                setNoticeOn(true);
-            }}/>
-            
-            <NavLink to={"/profile"}>
-                <img src="/profile.svg" alt="profile" className='profile-img icon-wiggle' />
-            </NavLink>
-        </div>
-    )
+    function falsifyProfileOn() {
+        setProfileOn(false);
+    }
 
+    const profileImgClick = (e) => {
+        e.stopPropagation(); // Prevent event capturing
+        setProfileOn((prevState) => !prevState);
+    };
+
+    useState(() => {
+        const clickOutside = (e) => {
+            if (profileRef.current && !profileRef.current.contains(e.target)) {
+                falsifyProfileOn();
+            }
+        };
+
+        document.addEventListener("click", clickOutside);
+
+        return () => {
+            document.removeEventListener("click", clickOutside);
+        };
+    }, []);
+
+    return (
+        <div className="bell-profile-box" onClick={() => setProfileOn(false)}>
+            {noticeOn ? (
+                <NoticeContainer falsifyNoticeOn={falsifyNoticeOn} />
+            ) : null}
+            <img
+                src="/notification.png"
+                alt="bell"
+                className="bell-img icon-wiggle"
+                onClick={() => {
+                    setNoticeOn(true);
+                }}
+            />
+            {profileOn ? (
+                <div ref={profileRef}>
+                    <ProfileContainer falsifyProfileOn={falsifyProfileOn} />
+                </div>
+            ) : null}
+            <img
+                src="/profile.svg"
+                alt="profile"
+                className="profile-img icon-wiggle"
+                onClick={profileImgClick}
+            />
+        </div>
+    );
 }
