28ce6b96612a36f2cec6c17a568860a57d9b9944
이지원 <dlwldnjs427@naver.com>
2023-10-04 14:21:56 +0900

    [Fix] 마이페이지 내 정보 받아오는 로직 추가
    Former-commit-id: ad2e137b9b409da35d6a1faf8c9a5b9938ee1f4d

--- fe/src/pages/Profile.js
+++ fe/src/pages/Profile.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Profile.css";
 import ProfileBanner from "../components/profileBanner/ProfileBanner";
 import ProfileWorks from "../components/profileWorks/ProfileWorks";
 import FilterSelect from "../components/filterSelect/FilterSelect";
+import axios from "axios";
 
 export default function Profile({ myData }) {
     const [modalOn, setModalOn] = useState(false);
+    const [commentNum, setCommentNum] = useState(0);
+    const [videoNum, setVideoNum] = useState(0);
 
     useState(() => {
         console.log(myData);
@@ -21,10 +24,38 @@ export default function Profile({ myData }) {
         }, 100);
     }
 
+    async function getMyData() {
+        try {
+            const response = await axios.get(
+                `${process.env.REACT_APP_API_PATH}/api/mypage/mydata`,
+                {
+                    headers: {
+                        Authorization:
+                            "Bearer " + localStorage.getItem("access_token"),
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            console.log(response.data);
+            setVideoNum(response.data.videoNum);
+            setCommentNum(response.data.commentNum);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getMyData();
+    }, []);
+
     return (
         <div className="profile-page">
             {modalOn ? <FilterSelect handleModalOff={handleModalOff} /> : null}
-            <ProfileBanner handleModalOn={handleModalOn} />
+            <ProfileBanner
+                handleModalOn={handleModalOn}
+                videoNum={videoNum}
+                commentNum={commentNum}
+            />
             <ProfileWorks />
         </div>
     );
