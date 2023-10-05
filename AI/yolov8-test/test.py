5b652f5223ddf84fd194cb051932ea6d8a496c98
dldntjr0516@gmail.com <dldntjr0516@gmail.com>
2023-10-05 15:38:18 +0900

    [Feat] 테스트 좀 했음
    Former-commit-id: 92f3998a1050aaf0e788b1a1443dc16ace367031

--- AI/yolov8-test/test.py
+++ AI/yolov8-test/test.py
@@ -14,10 +14,7 @@ def run():
     # Use the model
     # model.train(data="coco128.yaml", epochs=3, batch=8)  # train the model
     # metrics = model.val()  # evaluate model performance on the validation set
-    results = model.predict(source="./images/pistol_000.jpg", save=True) # predict on an image
-    results = model.predict(source="./images/pistol_001.jpg", save=True) # predict on an image
-    results = model.predict(source="./images/pistol_002.jpg", save=True) # predict on an image
-    results = model.predict(source="./images/pistol_003.jpg", save=True) # predict on an image
+    results = model.predict(source="./images/rifle_001.jpg", save=True) # predict on an image
     # path = model.export(format="onnx")  # export the model to ONNX format
     print(results)
 
