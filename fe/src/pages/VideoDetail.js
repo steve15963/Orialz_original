4e81328c8b14d8eda01df5a4586060991fe1ddbf
이지원 <dlwldnjs427@naver.com>
2023-10-05 09:50:20 +0900

    [Fix] 영상 상세정보 디자인
    Former-commit-id: 8e2d6291dfd57b3dc6f409a769f7844f50fdd486

--- fe/src/pages/VideoDetail.js
+++ fe/src/pages/VideoDetail.js
@@ -1,122 +1,150 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import axios from 'axios';
-import React, { useRef, useEffect, useState } from 'react';
-import VideoJS from '../components/VideoJS';
-import videojs from 'video.js';
-import './VideoDetail.css';
-import CommentsContainer from '../components/commentsContainer/CommentsContainer';
-import VideoSubs from '../components/videoSubs/VideoSubs';
+import axios from "axios";
+import React, { useRef, useEffect, useState } from "react";
+import VideoJS from "../components/VideoJS";
+import videojs from "video.js";
+import "./VideoDetail.css";
+import CommentsContainer from "../components/commentsContainer/CommentsContainer";
+import VideoSubs from "../components/videoSubs/VideoSubs";
 // import data3 from './data3.json';
 // import tempVideo from './tempVideo.mp4';
-require('videojs-contrib-hls.js');
+require("videojs-contrib-hls.js");
 
-const VideoDetail = ({videos}) => {
+const VideoDetail = ({ videos }) => {
+    const [curVideo, setCurVideo] = useState({});
+    const urlParams = new URL(window.location.href).searchParams;
+    const videoId = useRef(urlParams.get("id"));
 
-	const [curVideo, setCurVideo] = useState({});
-	const urlParams = new URL(window.location.href).searchParams;
-	const videoId = useRef(urlParams.get('id'));
-	
-	const blurUrlRef = useRef(`https://test.orialz.com/api/blur/list/7/11`);
-	const userInfoRef = useRef("ImUser");
+    const blurUrlRef = useRef(`https://test.orialz.com/api/blur/list/7/11`);
+    const userInfoRef = useRef("ImUser");
 
-	// const data4 = useRef(null);
+    // const data4 = useRef(null);
 
-	async function viewIncrease() {
-		try {
-			//응답 성공
-			await axios.get(`https://test.orialz.com/api/video/${videoId.current}/view`, {});
-		} catch (error) {
-			//응답 실패
-			console.error(error);
-		}
-	}
-	
-	
+    async function viewIncrease() {
+        try {
+            //응답 성공
+            await axios.get(
+                `https://test.orialz.com/api/video/${videoId.current}/view`,
+                {}
+            );
+        } catch (error) {
+            //응답 실패
+            console.error(error);
+        }
+    }
 
-	useEffect(()=>{
-		viewIncrease();
-		videos.forEach((e)=>{
-			if(e.id === Number(videoId.current)){
-				setCurVideo(e);
-				return;
-			}
-		});
+    useEffect(() => {
+        viewIncrease();
+        videos.forEach((e) => {
+            if (e.id === Number(videoId.current)) {
+                setCurVideo(e);
+                return;
+            }
+        });
+    }, []);
 
+    const playerRef = useRef(null);
 
-	},[]);
+    const videoJsOptionsRef = useRef(null);
+    videoJsOptionsRef.current = {
+        autoplay: true,
+        controls: true,
+        // responsive: true,
+        // fluid: true,
+        userActions: {
+            click: false,
+            doubleClick: false,
+        },
+        controlBar: {
+            pictureInPictureToggle: false,
+            fullscreenToggle: false,
+        },
+        sources: [
+            {
+                src: `https://test.orialz.com/hls/streaming/${videoId.current}/output.m3u8`,
+                type: "application/x-mpegURL",
+                // src: tempVideo,
+                // type: 'video/mp4'
+            },
+        ],
+    };
 
-	const playerRef = useRef(null);
+    const handlePlayerReady = (player) => {
+        playerRef.current = player;
 
-	const videoJsOptionsRef = useRef(null);
-	videoJsOptionsRef.current = {
-		autoplay: true,
-		controls: true,
-		// responsive: true,
-		// fluid: true,
-		userActions: {
-			click: false,
-			doubleClick: false,
-		},
-		controlBar:{
-			pictureInPictureToggle: false,
-			fullscreenToggle: false,
-		},
-		sources: [{
-			src: `https://test.orialz.com/hls/streaming/${videoId.current}/output.m3u8`,
-			type: 'application/x-mpegURL'
-			// src: tempVideo,
-			// type: 'video/mp4'
-		}]
-	};
-	
+        // You can handle player events here, for example:
+        player.on("waiting", () => {
+            videojs.log("player is waiting");
+        });
 
-	
-	const handlePlayerReady = (player) => {
-		playerRef.current = player;
-		
-		// You can handle player events here, for example:
-		player.on('waiting', () => {
-		  videojs.log('player is waiting');
-		});
-	
-		player.on('dispose', () => {
-		  videojs.log('player will dispose');
-		});
-	};
+        player.on("dispose", () => {
+            videojs.log("player will dispose");
+        });
+    };
 
-	
-	
-	return (
-		<div className="video-detail-page">
-			<div className="video-detail-container">
-				<div className="videojs-container">
-					<VideoJS options={videoJsOptionsRef.current} onReady={handlePlayerReady} blurUrl={blurUrlRef.current} userInfo={userInfoRef.current}/>
-				</div>
-				<VideoDisc curVideo={curVideo}/>
-				
-				<CommentsContainer videoId={videoId.current}/>
-				
-			</div>
-			<VideoSubs videos={videos}/>
-			
-		</div>
-	);
+    return (
+        <div className="video-detail-page">
+            <div className="video-detail-container">
+                <div className="videojs-container">
+                    <VideoJS
+                        options={videoJsOptionsRef.current}
+                        onReady={handlePlayerReady}
+                        blurUrl={blurUrlRef.current}
+                        userInfo={userInfoRef.current}
+                    />
+                </div>
+                <VideoDisc curVideo={curVideo} />
+
+                <CommentsContainer videoId={videoId.current} />
+            </div>
+            <VideoSubs videos={videos} />
+        </div>
+    );
 };
 
-function VideoDisc({curVideo}){
-	return(
-		<>
-			<div className="video-detail-title">
-				{curVideo.title}
-			</div>
-			<div className="video-detail-description">
-				<div>업로드 일: {curVideo.date}</div>
-				<div>올린이: {curVideo.uploader}</div>
-				<div>조회수: {curVideo.view}</div>
-				<div>설명:</div>
-			</div>
-		</>
-	)
+function VideoDisc({ curVideo }) {
+    const date = new Date(curVideo.date);
+    const formattedDate = date.toLocaleString("ko-KR", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+    });
+
+    return (
+        <>
+            <div className="video-detail-title">{curVideo.title}</div>
+            <div className="video-detail-description">
+                <div className="video-detail-uploader">
+                    {curVideo.uploaderProfile !== null ? (
+                        <img
+                            className="video-detail-uploaderprofile"
+                            src={curVideo.uploaderProfile}
+                            alt="uploader"
+                        />
+                    ) : (
+                        <img
+                            className="video-detail-uploaderprofile"
+                            src="basic-profile-logo.png"
+                            alt="uploader"
+                        />
+                    )}
+                    <div className="video-detail-uploadernickname">
+                        {curVideo.uploader}
+                    </div>
+                </div>
+
+                <div className="video-detail-viewndate">
+                    <div>조회수 {curVideo.view}회&nbsp;&nbsp;&nbsp;</div>
+                    <div>{formattedDate}</div>
+                </div>
+                <br />
+                <div>{curVideo.content}</div>
+            </div>
+        </>
+    );
 }
 
 export default VideoDetail;
