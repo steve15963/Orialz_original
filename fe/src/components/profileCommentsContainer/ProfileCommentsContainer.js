681fcdeaf9de40d5097578fbbf1b0d97b113de00
이지원 <dlwldnjs427@naver.com>
2023-10-06 09:00:55 +0900

    [Fix] 마이페이지 내 댓글 디자인 수정
    Former-commit-id: 6f2ff78226cb7cacfb306725eb45d5092e184e70

--- fe/src/components/profileCommentsContainer/ProfileCommentsContainer.js
+++ fe/src/components/profileCommentsContainer/ProfileCommentsContainer.js
@@ -1,15 +1,15 @@
-import CommentBox from '../commentBox/CommentBox'
+import ProfileCommentBox from "../profileCommentBox/ProfileCommentBox";
 
-export default function ProfileCommentsContainer({commentList}){
-
-    return(
-        <div className='profile-comments-container'>
-            {commentList.length > 0
-            ? commentList.map((comment, idx) => {
-                return <CommentBox comment={comment} key={idx} />;
-            })
-            : <div>작성한 댓글이 없습니다.</div>
-            }
+export default function ProfileCommentsContainer({ commentList }) {
+    return (
+        <div className="profile-comments-container">
+            {commentList.length > 0 ? (
+                commentList.map((comment, idx) => {
+                    return <ProfileCommentBox comment={comment} key={idx} />;
+                })
+            ) : (
+                <div>작성한 댓글이 없습니다.</div>
+            )}
         </div>
-    )
+    );
 }
