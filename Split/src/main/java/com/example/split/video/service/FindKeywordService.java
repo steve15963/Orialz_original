74475949665abd0f0a3231a46b24f0a07f2470cc
송진현 <dkdkdkal123@naver.com>
2023-10-07 10:25:56 +0900

    [Test] 우석 AI 연동 테스트
    Former-commit-id: 8610d9c3da2441dd05e477f8bd8f571a8855763a

--- Split/src/main/java/com/example/split/video/service/FindKeywordService.java
+++ Split/src/main/java/com/example/split/video/service/FindKeywordService.java
@@ -12,6 +12,7 @@ import com.example.split.video.Repository.FindKeywordRepository;
 import com.example.split.video.Repository.JobRepository;
 import com.example.split.video.controller.dto.response.AIData;
 import com.example.split.video.controller.dto.response.Prediction;
+import com.example.split.video.controller.dto.response.YOLOv8;
 import com.example.split.video.domain.Entity.FindKeyword;
 import com.example.split.video.domain.Entity.Job;
 import com.example.split.video.domain.Entity.Video;
@@ -45,27 +46,44 @@ public class FindKeywordService {
 		}
 	}
 	public void pushJson(double key, String json,Video video) throws JsonProcessingException {
-		AIData aiData = om.readValue(json, AIData.class);
-		Prediction prediction = aiData.getPredictions().get(0);
-		ArrayList<Integer> labels = prediction.getLabels();
-		ArrayList<ArrayList<Double>> bboxes = prediction.getBboxes();
-		ArrayList<Double> scores = prediction.getScores();
-		int size = labels.size();
+		//AIData aiData = om.readValue(json, AIData.class);
+		//Prediction prediction = aiData.getPredictions().get(0);
+		//ArrayList<Integer> labels = prediction.getLabels();
+		//ArrayList<ArrayList<Double>> bboxes = prediction.getBboxes();
+		//ArrayList<Double> scores = prediction.getScores();
+		//int size = labels.size();
+		YOLOv8 yolOv8 = om.readValue(json, YOLOv8.class);
+		ArrayList<ArrayList<Double>> xywhn = yolOv8.getXywhn();
+		ArrayList<Integer> cls = yolOv8.getCls();
+		int size = cls.size();
 		for(int i = 0; i < size; i++){
-			Integer label = labels.get(i);
-			ArrayList<Double> position = bboxes.get(i);
-			Double score = scores.get(i);
+			//Integer label = labels.get(i);
+			//ArrayList<Double> position = bboxes.get(i);
+			//Double score = scores.get(i);
 			//video_keywords_id, video_id, times, minx, miny, maxx, maxy, score
+			//findKeywordRepository.save(
+				//FindKeyword.builder()
+					//.keyword_id(label)
+					//.video(video)
+					//.time(key)
+					//.minX(position.get(0).intValue())
+					//.minY(position.get(1).intValue())
+					//.maxX(position.get(2).intValue())
+					//.maxY(position.get(3).intValue())
+					//.score(score)
+					//.build()
+			//);
+			ArrayList<Double> position = xywhn.get(i);
 			findKeywordRepository.save(
 				FindKeyword.builder()
-					.keyword_id(label)
+					.keyword_id(cls.get(i)+200)
 					.video(video)
 					.time(key)
-					.minX(position.get(0).intValue())
-					.minY(position.get(1).intValue())
-					.maxX(position.get(2).intValue())
-					.maxY(position.get(3).intValue())
-					.score(score)
+					.minX(position.get(0))
+					.minY(position.get(1))
+					.maxX(position.get(2))
+					.maxY(position.get(3))
+					.score(1)
 					.build()
 			);
 
